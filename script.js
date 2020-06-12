ad5840d78c5ef3ac42526ead3db840f3b3415de1
katmdev <katherinedevrieze@gmail.com>
2020-06-12 15:47:20 -0400

    ajax calls resolved, results displayed

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ galleryApp.search = 'search';
 galleryApp.object = 'objects/'
 
 galleryApp.getInput = () => {
+    galleryApp.artObjectArray = [];
     const userInput = $('input[name=search]').val();
     galleryApp.getArtIds(userInput);
 };
@@ -18,24 +19,49 @@ galleryApp.getArtIds = (queryValue) => {
             q: queryValue
         }
     }).then((data) => {
-        galleryApp.artObjectArray = [];
         data.objectIDs.forEach((id) => {
-            galleryApp.getArtObjects(id);
+            galleryApp.artObjectArray.push(galleryApp.getArtObjects(id));
         });
-        console.log(galleryApp.artObjectArray);
+        $.when(...galleryApp.artObjectArray).then((...artArray) => {
+            const artInfo = artArray.map((artInfoArray) => {
+                return artInfoArray[0];
+            });
+            galleryApp.displayThumbnails(artInfo);
+        });
     });
 };
 galleryApp.getArtObjects = (objectEndpoint) => {
-    $.ajax({
+    return $.ajax({
         url: `${galleryApp.url}${galleryApp.object}${objectEndpoint}`,
         method: 'GET',
         dataType: 'json'
-    }).then((data) => {
-        galleryApp.artObjectArray.push(data);
     });
 };
 
-galleryApp.displayThumbnails = () => {};
+galleryApp.displayThumbnails = (resultsArray) => {
+    console.log(resultsArray);
+    $('.gallery').empty();
+    resultsArray.forEach((result) => {
+        const objId = result.objectID;
+        const thumbnail = result.primaryImageSmall;
+        const image = result.primaryImage;
+        const medium = result.medium;
+        const date = result.objectDate;
+        const name = result.objectName;
+        const title = result.title;
+        const artist = result.artistDisplayName;
+        // error handling for variables that have empty string "unknown"
+        console.log(objId, thumbnail, image, medium, date, name, title, artist);
+        const img = $('<img>').attr({'src': thumbnail, 'alt': title})
+        const imageContainer = $('<div>').addClass('galleryThumbnail').attr('id', objId).html(img);
+        $('.gallery').append(imageContainer);
+    });
+
+
+    // append thumbnails from primaryImageSmall
+    // assign ID = objectID
+    // think about pagenation when displaying the thumbnails
+};
 galleryApp.addMoreResults = () => {};
 galleryApp.toggleInfo = () => {};
 galleryApp.displayLarger = () => {};
@@ -48,7 +74,7 @@ galleryApp.init = () => {
     $('#moreResults').on('click', function() {
 
     });
-    $('.galleryThumbnail').on('click', function() {
+    $('.gallery').on('click', '.galleryThumbnail', function() {
 
     });
     $('.info').on('click', function() {
