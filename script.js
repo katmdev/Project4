5df2f74f7bc595d8520dfbc495a288a6df8d537c
katmdev <katherinedevrieze@gmail.com>
2020-06-15 22:13:51 -0400

    finished accessibility features, added responsive elements, added comments

--- script.js
+++ script.js
@@ -5,14 +5,17 @@ galleryApp.search = 'search';
 galleryApp.object = 'objects/'
 
 galleryApp.getInput = () => {
+    // get user input, place loading image on page, pass input to ajax call
     const loading = $('<img>').attr('src', './assets/loading.gif');
     const placeholderContainer = $('<div>').addClass('placeholder').html(loading);
     $('.gallery').empty().append(placeholderContainer);
+    // create arrays for returned data
     galleryApp.artObjectArray = [];
     galleryApp.displayedArray = [];
     const userInput = $('input[name=search]').val();
     galleryApp.getArtIds(userInput);
 };
+
 galleryApp.getArtIds = (queryValue) => {
     $.ajax({
         url: `${galleryApp.url}${galleryApp.search}`,
@@ -22,16 +25,19 @@ galleryApp.getArtIds = (queryValue) => {
             isHighlight: true,
             q: queryValue
         }
-
     }).then((data) => {
+        // checks if query params return any results, if not, error message
         if (data.objectIDs) {
+            // runs second ajax with object IDs from first call to different endpoint
             data.objectIDs.forEach((id) => {
                 galleryApp.artObjectArray.push(galleryApp.getArtObjects(id));
             });
+            // formats promise to usable array
             $.when(...galleryApp.artObjectArray).then((...artArray) => {
                 galleryApp.artInfo = artArray.map((artInfoArray) => {
                     return artInfoArray[0];
                 });
+                // removes placeholder loading image
                 $('.gallery').empty();
                 galleryApp.displayThumbnails(galleryApp.artInfo);
             });
@@ -51,6 +57,8 @@ galleryApp.getArtObjects = (objectEndpoint) => {
 };
 
 galleryApp.displayThumbnails = (resultsArray) => {
+    // removes appended items from returned array and creates reference array for caption
+    // displays results on page
     galleryApp.splicedArray = resultsArray.splice(0, 20);
     galleryApp.splicedArray.forEach((result) => {
         galleryApp.displayedArray.push(result);
@@ -59,16 +67,20 @@ galleryApp.displayThumbnails = (resultsArray) => {
         const name = result.objectName;
         const title = result.title;
         const img = $('<img>').attr({ 'src': image, 'alt': `${title}, ${name}`, 'id': `image${objId}`}).addClass('galleryImage');
-        const imageContainer = $('<div>').addClass('galleryThumbnail').attr('id', objId).html(img);
+        const imageContainer = $('<button>').addClass('galleryThumbnail').attr('id', objId).html(img);
         $('.gallery').append(imageContainer); 
     });
+    // checks results array to see if more items need to be added
     if (resultsArray.length > 0) {
-        const showMore = $('<button>').addClass('showMore').text('more');
-        $('.gallery').append(showMore);
+        const showMoreClass = "fas fa-chevron-down"
+        const showMore = $('<i>').addClass(showMoreClass);
+        const showMoreButton = $('<button>').addClass('showMore').val("Show more").html(showMore);
+        $('.gallery').append(showMoreButton);
     };
 };
 
 galleryApp.displayLarger = (idNumber) => {
+    // displays larger image in highlight container
     galleryApp.displayedArray.forEach((displayedObject) => {
         if (idNumber == displayedObject.objectID) {
         const obj = {
@@ -81,6 +93,7 @@ galleryApp.displayLarger = (idNumber) => {
             artist: displayedObject.artistDisplayName
         };
         for (variable in obj) {
+            // error handling for undefined fields returned from ajax call
             if (!obj[variable]) {
                 obj[variable] = `${variable} unknown`
             }
@@ -88,7 +101,7 @@ galleryApp.displayLarger = (idNumber) => {
         const info = `fas fa-info-circle`;
         const img = $('<img>').attr({'src': obj.image, 'alt': `${obj.title}, ${obj.name}`})
         const imageContainer = $('<div>').addClass('highlight__container').html(img);
-        const icon = $('<i>').addClass(info);
+        const icon = $('<i>').addClass(info).attr('id', 'icon');
         const infoButton = $('<button>').val(obj.objId).addClass('infoButton').html(icon);
         const title = $('<p>').text(obj.title);
         const artist = $('<p>').text(obj.artist);
@@ -101,9 +114,7 @@ galleryApp.displayLarger = (idNumber) => {
         };
     });
 };
-// galleryApp.toggleInfo = (iconId) => {
-//     console.log(iconId);
-// };
+
 
 galleryApp.init = () => {
     $('form').on('submit', function(e) {
@@ -120,8 +131,8 @@ galleryApp.init = () => {
     });
     $('.highlight').on('click', '.infoButton', function(e) {
         e.preventDefault();
-        $('.fas').toggleClass('fa-times-circle fa-info-circle');
-        $('.caption').toggle();
+        $('#icon').toggleClass('fa-times-circle fa-info-circle');
+        $('.caption').slideToggle();
     });
 };
 
